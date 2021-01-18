f9f2e7bca1edc7d951c6366b96960c26a9222a03
Saki Jono <kinokinoartist12@gmail.com>
2021-01-18 14:25:53 +0900

    favicon add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Machibito</title>
+        <link rel="icon" href="machibito.ico">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
